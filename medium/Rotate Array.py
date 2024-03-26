b5352e7c3bd9c56a3891c9873a0003f61c0daf50
Aryan Sharma <sharmaaryan6383@gmail.com>
2024-03-26 15:29:18 +0530

    medium commit

---
+++ medium/Rotate Array.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/rotate-array/
+
+class Solution(object):
+    def rotate(self, nums, k):
+        n = len(nums)
+        k = k % n 
+
+        nums.reverse()
+        nums[:k] = reversed(nums[:k])
+
+        nums[k:] = reversed(nums[k:])
